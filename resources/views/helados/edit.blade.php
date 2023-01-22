7474668839e90178786efc10bfc8baffff1e39f2
AryDaniel <ary.lopez@cetis107.edu.mx>
2023-01-21 23:17:13 -0700

    Editar registro - visualizar datos

--- resources/views/helados/edit.blade.php
+++ resources/views/helados/edit.blade.php
@@ -3,20 +3,20 @@
 <div class="container bg-warning ">
     <div class="row">
         <div class="col-12">
-            <h1>Registrar Usuario</h1><hr>
+            <h1>Editar Helado</h1><hr>
             <form method="post" action="{{ route('heladera.update') }}">
                 @csrf @method('PATCH')
 
                 <br>
                 <div class="form-group">
-                    <input type="text" class="form-control" name="nombre" placeholder="Teclea el nombre del producto">
+                    <input type="text" class="form-control" name="nombre" placeholder="Teclea el nombre del producto" value="{{$helado->nombre}}">
                 </div>
 
                 <br>
                 <div class="form-row align-items-center">
                     <div class="col-auto">
                         <label class="sr-only" for="inlineFormInput">Sabor</label>
-                        <input name="sabor" type="text" class="form-control mb-2" id="inlineFormInput" placeholder="Teclea un sabor">
+                        <input name="sabor" type="text" class="form-control mb-2" id="inlineFormInput" placeholder="Teclea un sabor" value="{{$helado->sabor}}">
                     </div>
                     <div class="col-auto">
                         <label class="sr-only" for="inlineFormInputGroup">Username</label>
@@ -24,7 +24,7 @@
                             <div class="input-group-prepend">
                                 <div class="input-group-text">$</div>
                             </div>
-                            <input name="precio" type="number" class="form-control" id="inlineFormInputGroup" placeholder="Precio">
+                            <input name="precio" type="number" class="form-control" id="inlineFormInputGroup" placeholder="Precio" value="{{$helado->precio}}">
                         </div>
                     </div>
                 </div>
@@ -32,7 +32,7 @@
                 <br>
                 <div>
                     <a href=" {{ route('heladera.index') }}" type="button" class="btn btn-secondary">Regresar    </a>
-                    <input type="submit" class="btn btn-info" value="Registrar">
+                    <input type="submit" class="btn btn-info" value="Editar">
                 </div>
                 <br>
             </form>
--- resources/views/helados/show.blade.php
+++ resources/views/helados/show.blade.php
@@ -16,7 +16,7 @@
                     <hr class="my-4">
                     <div class="btn-group" role="group" aria-label="Basic example">
                         <a href=" {{ route('heladera.index') }}" type="button" class="btn btn-secondary">Regresar    </a>
-                        <a href="#" type="button" class="btn btn-info">Editar</a>
+                        <a href="{{ route('heladera.edit', $helado->id) }}" type="button" class="btn btn-info">Editar</a>
                         {{--<a href="{{ route('heladera.delete') }}" type="button" class="btn btn-danger">Eliminar</a> --}}
                         <form action="{{ route('heladera.delete', $helado) }}" method="POST"> {{-- Muy importante que sea metodo POST --}}
                             @csrf @method('DELETE')
